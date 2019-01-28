b4d522ce92c4faab9ab02c5e9f73a93c3dce287d
SeanGillespie <gillespi20@up.edu>
2019-01-27 20:22:01 -0800

    all files work as specified in sprint 0
    complete code for sprint 0

---
+++ js/functions.js
@@ -0,0 +1,3 @@
+function add(a , b) {
+    return Number(a) + Number(b);   // The function returns the sum of p1 and p2
+}
---
+++ js/index.js
@@ -0,0 +1,10 @@
+function setup() {
+    var p1 = document.getElementById("first").value;
+    var p2 = document.getElementById("second").value;
+    var a = add(p1, p2);
+    var res = document.createElement("P");
+    document.getElementById("demo").appendChild(res);
+    var t = document.createTextNode("" + a);
+    res.appendChild(t);                                          
+    document.body.appendChild(res);
+}
