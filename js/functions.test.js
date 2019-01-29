bf2d85e97427fabdcb1b137ba31f2d92393a72cf
SeanGillespie <gillespi20@up.edu>
2019-01-28 16:25:53 -0800

    Travis file fixed,tests will pass
    Travis file added stuff, tests both should pass!!

--- js/functions.test.js
+++ js/functions.test.js
@@ -1,9 +1,9 @@
 const add = require('./functions.js');
 
-test('add function is working', () => {
+test('2 plus 2 is 4', () => {
   expect(add(2, 2)).toBe(4);
 });
 
-test('add function is Failing', () => {
-  expect(add(2, 17)).toBe(4);
+test('2 + 3 = 5', () => {
+  expect(add(2, 3)).toBe(5);
 });
