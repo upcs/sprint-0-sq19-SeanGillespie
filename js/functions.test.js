090ee73f66a71f282ae227dd0c74a7cb0bb698d2
SeanGillespie <gillespi20@up.edu>
2019-01-28 17:16:57 -0800

    Update functions.test.js
    Test will now fail

--- js/functions.test.js
+++ js/functions.test.js
@@ -4,6 +4,6 @@ test('2 plus 2 is 4', () => {
   expect(add(2, 2)).toBe(4);
 });
 
-test('2 + 3 = 5', () => {
-  expect(add(2, 3)).toBe(5);
+test('2 + 3 = 7', () => {
+  expect(add(2, 3)).toBe(7);
 });
